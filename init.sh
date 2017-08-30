8de76cbacebb5bf3f7c0e273882d78b6fa3f6fe7
naziml <naziml@gmail.com>
2017-08-30 00:28:49 -0700

    spew logs to disk as well

--- init.sh
+++ init.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 service ssh start
+mkdir -p /var/logs
 nginx &
 for (( ; ; ))
 do
-   >&2 echo "$(date) I am still here ... "
+   >&2 echo "$(date) I am still here ... " | tee -a /var/logs/app.log
    echo "Hi there! I am just writing out a bunch of stuff here to fill up logs."
    
-   echo "What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? "
-   echo "What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? "
-   echo "What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? "
-   echo "What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? "
+   echo "What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? " | tee -a /var/logs/app.log
+   echo "What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? " | tee -a /var/logs/app.log
+   echo "What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? " | tee -a /var/logs/app.log
+   echo "What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? What do we have here? " | tee -a /var/logs/app.log
 
    sleep 10
 done
